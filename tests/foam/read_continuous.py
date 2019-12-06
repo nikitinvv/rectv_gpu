DEV: fix tomogarphy gradient steps

tests/foam/read_continuous.py
@@ -10,8 +10,8 @@ import h5py
 import tomopy
 import dxchange
 
-h5fname = "/home/beams/VNIKITIN/3dtv/dk_MCFG_1_p_s1_.h5"
-sino = (1300, 1316) # slices for reconstructions
+h5fname = "/data/staff/tomograms/viknik/rajmund/dk_MCFG_1_p_s1_.h5"
+sino = (1300, 1364) # slices for reconstructions
 nframes = 8 # time frames for reconstruction
 frame = 94 # middle time frame for reconstruction
 nproj = 300 # number of angles for 180 degrees interval
@@ -21,8 +21,6 @@ proj, flat, dark, theta = dxchange.read_aps_32id(h5fname, sino=sino, proj=((fram
 theta = theta[(frame-nframes//2)*nproj:(frame+nframes//2)*nproj]
 print(proj.shape)
 print(theta.shape)
-
-#proj = proj[(frame-nframes//2)*nproj:(frame+nframes//2)*nproj, :, :]
 
 # Flat-field correction of raw data.
 data = tomopy.normalize(proj, flat, dark, cutoff=1.4)
@@ -43,5 +41,5 @@ if data.shape[1] > 1:
     data = tomopy.downsample(data, level=binning, axis=1)
 
 # reshape for 4d
-data = np.reshape(data,[nframes,nproj,data.shape[1],data.shape[2]])
-np.save('data.npy',data)
+data = np.reshape(data,[nframes*nproj,data.shape[1],data.shape[2]])
+np.save('foambin2.npy',data.swapaxes(0,1))

tests/foam/rec.py
@@ -4,16 +4,15 @@
 import rectv_gpu
 import numpy as np
 import dxchange
-import tomopy
 import sys
 
 
 def getp(a):
     return a.__array_interface__['data'][0]
 
-def takephi(m, ntheta):
+def takephi(ntheta):
+    m = 8  # number of basis functions
     [x, y] = np.meshgrid(np.arange(-ntheta//2, ntheta//2), np.arange(-m//2, m//2))
-    # phi = np.exp(2*np.pi*1j*x*y/ntheta)/np.sqrt(ntheta)
     phi = np.zeros([m, 2*ntheta], dtype='float32')
     phi[:, ::2] = np.cos(2*np.pi*x*y/ntheta)/np.sqrt(ntheta)
     phi[:, 1::2] = np.sin(2*np.pi*x*y/ntheta)/np.sqrt(ntheta)
@@ -22,28 +21,29 @@ def takephi(m, ntheta):
 
 if __name__ == "__main__":
 
-    data = np.load("data2.npy")  # load continuous data
+    data = np.load("foambin2.npy")
     [ns, ntheta, n] = data.shape
-    rot_center = n/2
-   
-    m = 8  # number of basis functions, must be a multiple of nframes
-    lambda0 = 1e-3  # regularization parameter 1
-    lambda1 = 1  # regularization parameter 2
-    nsp = np.int(sys.argv[1]) # number of slices to process simultaniously by gpus
-    ngpus = np.int(sys.argv[2]) # number of gpus
-    niter = np.int(sys.argv[3])  # number of iterations
-    titer = np.int(sys.argv[4])  # number of iterations
+    rot_center = n//2 # rotation center
+    lambda0 = 2e-3  # regularization parameter 1
+    lambda1 = 2  # regularization parameter 2
+    nsp = 4 # number of slices to process simultaniously by gpus
+    ngpus = 4 # number of gpus
+    niter = 64  # number of ADMM iterations
+    titer = 4  # number of inner tomography iterations
     
+    # take basis functions for decomosition 
+    phi = takephi(ntheta) 
+    m = phi.shape[0] # number of basis functions
+    # creaate class for processing
     cl = rectv_gpu.rectv(n, ntheta, m, ns,
                          nsp, ngpus, rot_center, lambda0, lambda1)
     # angles
-    theta = np.linspace(0, 8*np.pi, ntheta, endpoint=False).astype('float32')
-    # basis
-    phi = takephi(m, ntheta)
+    theta = np.linspace(0, 8*np.pi, ntheta, endpoint=False).astype('float32')  
     # memory for result
     rtv = np.zeros([ns,m,n,n], dtype='float32')
     # Run iterations
-    cl.run(getp(rtv), getp(data), getp(theta), getp(phi),  niter, titer)
-    print(np.linalg.norm(rtv))
+    dbg = False # show relative convergence
+    cl.run(getp(rtv), getp(data), getp(theta), getp(phi),  niter, titer, dbg)
+    # Save result
     for k in range(rtv.shape[0]):
-        dxchange.write_tiff_stack(rtv[k], 'rec_tv/rec_'+str(k), overwrite=True)
+         dxchange.write_tiff_stack(rtv[k], 'rec_tv/rec_'+str(k), overwrite=True)